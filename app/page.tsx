10f5f0f8f3b8157b51a737fefaecf6654b558377
Andrei Rizea <andrei@indus-agency.com>
2025-11-21 20:31:00 +0000

    fixing bugs

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import HeroSection from '@/components/hero-section'
 import Features from '@/components/features-4'
 import Footer from '@/components/footer'
 import FAQsTwo from '@/components/faqs-2'
+import Pricing from '@/components/pricing'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <HeroSection />
       <Features />
       <FAQsTwo />
+      <Pricing />
       <Footer />
     </>
   )
