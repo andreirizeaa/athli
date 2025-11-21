a5ceec4c60049a1a9c59fe3d34357a61c3239150
Andrei Rizea <andrei@indus-agency.com>
2025-11-21 16:14:31 +0000

    Base landing page template

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,18 @@ import { Authenticated, Unauthenticated } from 'convex/react'
 import { useQuery } from 'convex/react'
 import { api } from '../convex/_generated/api'
 import { useTranslations } from 'next-intl'
+import HeroSection from '@/components/hero-section'
+import Features from '@/components/features-4'
+import Footer from '@/components/footer'
+import FAQsTwo from '@/components/faqs-2'
 
 export default function Home() {
   return (
     <>
-      <Authenticated>
-        <UserButton />
-        <Content />
-      </Authenticated>
-      <Unauthenticated>
-        <SignInButton />
-      </Unauthenticated>
+      <HeroSection />
+      <Features />
+      <FAQsTwo />
+      <Footer />
     </>
   )
 }
