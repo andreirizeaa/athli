621e5f76fe9066cfb874c06fc4629d9f466afa3a
Andrei Rizea <andrei@indus-agency.com>
2025-11-23 08:51:09 +0000

    Adding new sub pages

---
+++ app/app/athletes/[clientId]/training-calendar/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+const ClientTrainingCalendarPage = () => {
+  return (
+    <div className="flex h-full w-full items-start justify-start">
+      <p>Training Calendar</p>
+    </div>
+  )
+}
+
+export default ClientTrainingCalendarPage
+
+
