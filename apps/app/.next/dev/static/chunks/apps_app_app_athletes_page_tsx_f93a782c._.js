90a7675d6366f07d8e7c1795c338880d6099632c
Andrei Rizea <andrei@indus-agency.com>
2025-11-25 13:45:42 +0000

    updating git ignore

--- apps/app/.next/dev/static/chunks/apps_app_app_athletes_page_tsx_f93a782c._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/apps_app_4855a96b._.js",
-  "static/chunks/node_modules_20c66add._.js"
-],
-    source: "dynamic"
-});
--- apps/app/.next/dev/static/chunks/apps_app_app_library_layout_tsx_f93a782c._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_b0523ead._.js"
-],
-    source: "dynamic"
-});
