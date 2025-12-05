0450fb1c0bcbbd3bae85afe77ae133a2c05f9e37
Andrei Rizea <andrei@indus-agency.com>
2025-12-05 19:57:30 +0000

    error pages

--- apps/app/lib/utils.ts
+++ apps/app/lib/utils.ts
@@ -1,6 +1,25 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import type { Metadata } from 'next';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function generateMeta({
+  title,
+  description,
+  canonical
+}: {
+  title: string;
+  description: string;
+  canonical: string;
+}): Metadata {
+  return {
+    title: `${title} - OneNinety`,
+    description: description,
+    alternates: {
+      canonical: canonical
+    }
+  };
 }
