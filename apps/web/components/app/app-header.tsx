9c5889410579b33975c3333d2a021ba9b6ea8daa
Andrei Rizea <andrei@indus-agency.com>
2026-02-15 17:27:57 +0000

    changes

--- apps/web/components/app/app-header.tsx
+++ apps/web/components/app/app-header.tsx
@@ -131,7 +131,7 @@ export function AppHeader({
             </Button>
           </div>
           {/* Trial badge - hidden on mobile */}
-          {false && isOnTrial && (
+          {isOnTrial && (
             <Tooltip>
               <TooltipTrigger asChild>
                 <Link
